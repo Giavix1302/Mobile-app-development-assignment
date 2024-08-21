c23e51571bd44dfdf83e08f79e4d8537bed1e1c0
H51M14\Student <Student@gmail.com>
2024-08-21 08:44:51 +0700

    add: code challenge 3

--- javascript-part1/coding-challenge3/main.js
+++ javascript-part1/coding-challenge3/main.js
@@ -1,12 +1,23 @@
-const dolphins = [96, 108, 89]
-const koalas = [88, 91, 110]
+const dolphins = [97, 112, 101]
+const koalas = [109, 95, 123]
 
 
 function handleAverageScores(scores) {
-    let avgScores = scores.reduce((total, score) => {
-        return (total + score) / 2
-    }, 0)
-    return avgScores;
+    let avgScores = 0;
+    scores.forEach(element => {
+        avgScores += element
+    });
+    return (avgScores / scores.length);
 }
 
-console.log(handleAverageScores(dolphins));
+if (handleAverageScores(dolphins) < 100 && (handleAverageScores(koalas) < 100)) {
+    console.log("noteam win!!");
+} else {
+    if (handleAverageScores(dolphins) === handleAverageScores(koalas)) {
+        console.log("two team draw!!")
+    } else if (handleAverageScores(dolphins) > handleAverageScores(koalas)) {
+        console.log("dolphins win!!")
+    } else {
+        console.log("koalas win!!")
+    }
+}
