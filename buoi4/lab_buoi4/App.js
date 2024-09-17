818649e4c8d7584375e8d5552f0b3b8a8d53562a
Giavix1302 <nguyenhoanggiavi123@gmail.com>
2024-09-17 21:50:00 +0700

    tuan4

---
+++ buoi4/lab_buoi4/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import DemoUseState from './src/Screen/DemoUseState';
+import Login from './src/Screen/login2a';
+import FeedBack2b from './src/Screen/FeedBack2b';
+import PassWord from './src/Screen/PassWord';
+import Tiki from './src/Screen/Tiki';
+
+export default function App() {
+  
+  return <Tiki/>
+}
